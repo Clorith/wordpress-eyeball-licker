7863145ba3d41a266783ec2be62ba3010a86655b
Marius <funman@gmail.com>
2013-07-18 14:04:00 +0200

    Added IP filters to only allow one lick per user on a post Added code commenting

--- eyeball.php
+++ eyeball.php
@@ -24,11 +24,26 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+/**
+ * Function for inserting an eyeball licking form after the_content thanks to the_content hook
+ *
+ * @param $content
+ * @return string
+ */
 function eye_post_suffix( $content ) {
+    /**
+     * We need post details, so we globalize the $post variable
+     */
     global $post;
 
+    /**
+     * Get the current amount of licks
+     */
     $licks = get_post_meta( $post->ID, 'eyeball_licks', true );
 
+    /**
+     * Output our form for licking, we also check if this post has been licked before, if it has add the lick count
+     */
     $content .= '
         <form action="" method="post">
             <input type="hidden" name="eyeball_post" value="' . $post->ID . '" />
@@ -44,18 +59,59 @@ function eye_post_suffix( $content ) {
     return $content;
 }
 
+/**
+ * The eye_post_form_handler is triggered by the "init" hook to run on every page
+ *
+ * It will only perform an action if our form has been submitted for eyeballs
+ */
 function eye_post_form_handler() {
     if ( isset( $_POST['eyeball_post'] ) )
     {
+        /**
+         * $licks holds the post's meta data for amount of licks to date
+         * $lickers are the users who has already licked this eyeball
+         */
         $licks = get_post_meta( $_POST['eyeball_post'], 'eyeball_licks', true );
+        $lickers = get_post_meta( $_POST['eyeball_post'], 'eyeball_lickers', true );
+
+        /**
+         * If the $licks meta hasn't been set before, it will be empty, we set it to 0 if it's new
+         */
         if ( empty( $licks ) )
             $licks = 0;
 
         $licks++;
 
+        /**
+         * If $lickers isn't set already (new eyeball), make it into an array
+         * If it has data, unserialize that data (DB stored array)
+         */
+        if ( empty( $lickers ) )
+            $lickers = array();
+        else
+            $lickers = unserialize( $lickers );
+
+        /**
+         * If the current IP has already licked the eyeball, we don't want to do anything, they only get one lick each!
+         */
+        if ( in_array( $_SERVER['REMOTE_ADDR'], $lickers ) )
+            return;
+
+        /**
+         * The user hasnt' licked this one before, great, add him to the licked list
+         */
+        $lickers[] = $_SERVER['REMOTE_ADDR'];
+
+        /**
+         * Update the post metas, $lickers is serialized as it is an array and we want to retain the data type
+         */
+        update_post_meta( $_POST['eyeball_post'], 'eyeball_lickers', serialize( $lickers ) );
         update_post_meta( $_POST['eyeball_post'], 'eyeball_licks', $licks );
     }
 }
 
+/**
+ * Our hooks, attaching to WordPress events
+ */
 add_action( 'the_content', 'eye_post_suffix' );
 add_action( 'init', 'eye_post_form_handler' );
